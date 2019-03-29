4c23e9b5c8b0ee892d369d263d8df4068ddcc25a
Nelia Terrazas <terrazasnelia@gmail.com>
2019-03-29 13:07:00 -0700

    install bootstrap and continue work on navbar

--- site/src/app/navbar/navbar.ts
+++ site/src/app/navbar/navbar.ts
@@ -1,10 +1,4 @@
-// export class Recipe {
-//   // public made: boolean = false;
-//   // constructor(public title: string, public ingredients: string[], public instructions: string) {}
-//   // markMade(){
-//   //   this.made = true;
-//   // }
-//   // markNotMade(){
-//   //   this.made = false;
-//   // }
+// export class NavbarComponent {
+//   nav
+//
 // }
