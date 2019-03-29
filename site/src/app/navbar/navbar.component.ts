aff2495b439b80c4b159e436d0a028957ec76dab
Nelia Terrazas <terrazasnelia@gmail.com>
2019-03-29 14:33:53 -0700

    debug './src/polyfills.ts' errors'

--- site/src/app/navbar/navbar.component.ts
+++ site/src/app/navbar/navbar.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  navbarOpen = false;
+  toggleNavbar() {
+  this.navbarOpen = !this.navbarOpen;
+  }
+  constructor(public navTitle: string, public navDescription: string) { }
+  listings: NavbarComponent[] = [
+    new NavbarComponent('BARNEYS WAREHOUSE', 'Shop Designer Steals at Barneys Warehouse!')
+  ]
 
-  constructor() { }
 
   ngOnInit() {
   }
