587aa41c7fa84d662afc3de408a2438b2fac48c8
Nelia Terrazas <terrazasnelia@gmail.com>
2019-03-29 12:00:07 -0700

    add navbar.ts to add to the navbar

---
+++ site/src/app/navbar/navbar.ts
@@ -0,0 +1,10 @@
+// export class Recipe {
+//   // public made: boolean = false;
+//   // constructor(public title: string, public ingredients: string[], public instructions: string) {}
+//   // markMade(){
+//   //   this.made = true;
+//   // }
+//   // markNotMade(){
+//   //   this.made = false;
+//   // }
+// }
